265022793470045405a5e5d11c52c83bf4e188f3
Filipe Ferreira <filipe.ferreira@sxgrupo.com>
2023-09-21 13:44:08 -0300

    Atualizações

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
+    //Inserir e retirar div da tela
+    var div = document.getElementById('main');
+    var button = document.getElementById('cadastro');
+    var bool = true;
+    button.addEventListener('click', function Inserir(){
+        if(bool){
+            div.innerHTML = `<div class="container-popup">
+        <div class="pop-form">
+            <h3 style="text-align: center; margin: 30px;">Insira seus dados para se cadastrar</h3>
+            <form action="submit">
+                <input type="text" placeholder="CPF" id="form_cpf">
+                <input type="text" placeholder="Nome completo" id="form_name">
+                <input type="text" placeholder="E-mail" id="form_email">
+                <input type="text" placeholder="Telefone" id="form_telefone">
+                <button type="submit" class="form-btn">Avançar</button>
+            </form>
+        </div>
+    </div>`;
+    bool = false;
+        }
+        else{
+            div.innerHTML = ``;
+            bool = true;
+        }
+    });
+
+    // Dados do formulário
     var cadastros = [
         {
             cpf: 14463167698,
@@ -6,7 +33,14 @@ document.addEventListener('DOMContentLoaded', function () {
             email: 'filipefdiniz30@gmail.com',
             telefone: 31993273796
         },
-        
+        {
+            cpf: 90510224687,
+            nome: 'Fabio Luiz Alves Diniz',
+            email: 'fabioluizalves@gmail.com',
+            telefone: 31991650801
+        },
+
+
     ];
 
     var inputCPF = document.getElementById('form_cpf');
@@ -25,6 +59,8 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
-
     
+    
+
+
 });
